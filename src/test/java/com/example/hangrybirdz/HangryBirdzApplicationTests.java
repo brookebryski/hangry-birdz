89ef3f48a2fd929a9f261435f4d3d3d03642da98
BrookeBryski <brookeabryski@gmail.com>
2021-11-09 12:37:04 -0600

    Initial commit

---
+++ src/test/java/com/example/hangrybirdz/HangryBirdzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangrybirdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangryBirdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

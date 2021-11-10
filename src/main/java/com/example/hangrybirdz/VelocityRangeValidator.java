c1e7fac281197dbfd2b592451006993168595097
MathiasAnderson <mathias.anderson@saggezza.com>
2021-11-09 20:12:25 -0600

    refactored if statement in VelocityRangeValidator

--- src/main/java/com/example/hangrybirdz/VelocityRangeValidator.java
+++ src/main/java/com/example/hangrybirdz/VelocityRangeValidator.java
@@ -4,10 +4,6 @@ public class VelocityRangeValidator implements IVelocityRangeValidator{
 
     @Override
     public boolean isInRange(int velocity) {
-        if(velocity >= 1 && velocity <= 20){
-            return true;
-        } else {
-            return false;
-        }
+        return velocity >= 1 && velocity <= 20;
     }
 }

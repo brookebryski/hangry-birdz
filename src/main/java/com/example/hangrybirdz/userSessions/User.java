83b3f6b56254921b3dd994c08c5ac507ea389c9f
BrookeBryski <brookeabryski@gmail.com>
2021-11-16 14:23:10 -0600

    userTests

--- src/main/java/com/example/hangrybirdz/userSessions/User.java
+++ src/main/java/com/example/hangrybirdz/userSessions/User.java
@@ -2,6 +2,8 @@ package com.example.hangrybirdz.userSessions;
 
 public class User implements IUser {
     private userController _userController;
+    private boolean _existingUser;
+    private String _username;
 
     public User(userController userController) {
         _userController = userController;
@@ -9,6 +11,24 @@ public class User implements IUser {
 
 
     public void initialize(String username){
-        _userController.read(username);
+        this._username = username;
+        if(_userController.read(username)) {
+            _existingUser = true;
+        } else {
+            _existingUser = false;
+        }
+    }
+
+    public void save(int score){
+        _userController.create(_username, score);
+        _existingUser = true;
+    }
+
+    public boolean isExistingUser() {
+        return _existingUser;
+    }
+
+    public String getUsername() {
+        return _username;
     }
 }
--- src/test/java/com/example/hangrybirdz/userSession/UserTest.java
+++ src/test/java/com/example/hangrybirdz/userSession/UserTest.java
@@ -5,12 +5,25 @@ import com.example.hangrybirdz.userSessions.User;
 import com.example.hangrybirdz.userSessions.userController;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class UserTest {
+
     @Test
-    public void givenUserNameCallReadOnce(){
-        //    - Given: I am a user
+    public void givenInitializeSetUsername() {
+        // - Given: I am a user
+        userController crud = mock(userController.class);
+        IUser user = new User(crud);
+        //When: I enter my username "dustin"
+        user.initialize("dustin");
+        assertEquals(user.getUsername(), "dustin");
+        //- Then: getUsername equals "dustin"
+    }
+
+    @Test
+    public void givenUserNameCallReadOnce() {
+        // - Given: I am a user
         userController crud = mock(userController.class);
         IUser user = new User(crud);
         //- When: I enter my name
@@ -18,6 +31,53 @@ public class UserTest {
         verify(crud, times(1)).read("dustin");
         //- Then: Call CRUD object.read
     }
-//    @Test
-//    public void givenUser
+
+    @Test
+    public void givenExistingUserIsExistingUserTrue() {
+        // - Given: I am an existing user
+        userController crud = mock(userController.class);
+        IUser user = new User(crud);
+        //- CRUD object.read
+        when(crud.read("dustin")).thenReturn(true);
+        user.initialize("dustin");
+        assertTrue(user.isExistingUser());
+        //- Then: IsExistingUser returns true
+    }
+
+    @Test
+    public void givenNewUserIsExistingUserFalse() {
+        // - Given: I am a new user
+        userController crud = mock(userController.class);
+        IUser user = new User(crud);
+        //- CRUD object.read
+        when(crud.read("dustin")).thenReturn(false);
+        user.initialize("dustin");
+        assertFalse(user.isExistingUser());
+        //- Then: IsExistingUser returns false
+    }
+
+    @Test
+    public void givenGameOverCallSaveOnce() {
+        // - Given: I am a user
+        userController crud = mock(userController.class);
+        IUser user = new User(crud);
+        //- When: The game ends
+        user.initialize("dustin");
+        user.save(5);
+        verify(crud, times(1)).create("dustin", 5);
+        //-  Then: CRUD object.create (user.username, user.score)
+    }
+
+    @Test
+    public void givenGameOverExistingUserTrue() {
+        // - Given: I am a user
+        userController crud = mock(userController.class);
+        IUser user = new User(crud);
+        //- When: The game ends
+        when(crud.read("dustin")).thenReturn(false);
+        user.initialize("dustin");
+        user.save(5);
+        assertTrue(user.isExistingUser());
+        //-  Then: isExistingUser returns true;
+    }
 }

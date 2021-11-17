d29095fe6f0729f2b85355827ff7f4b1136e86ef
JohnLoveland <hirejohnloveland@gmail.com>
2021-11-17 11:54:53 -0600

    Working Application please do not break me version final

--- src/main/java/com/example/hangrybirdz/gameplay/AppConfig.java
+++ src/main/java/com/example/hangrybirdz/gameplay/AppConfig.java
@@ -2,6 +2,7 @@ package com.example.hangrybirdz.gameplay;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
--- src/test/java/com/example/hangrybirdz/userSession/StatsTest.java
+++ src/test/java/com/example/hangrybirdz/userSession/StatsTest.java
@@ -1,12 +1,15 @@
 package com.example.hangrybirdz.userSession;
 
 import com.example.hangrybirdz.userSessions.*;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class StatsTest {
@@ -52,18 +55,69 @@ public class StatsTest {
     }
 
     @Test
-    public void GivenTestList() {
-//        Given - I am a new user
+    public void GivenTestListTotalGamesPlayedEqualsFour() {
+//        Given - I am an existing user
         when(user.isExistingUser()).thenReturn(true);
         when(user.getUsername()).thenReturn("bob");
         when(userController.readStats(anyString())).thenReturn(testList);
-//        When - I need to see stats
-        stats.getStats();
-//        Then - Do not Call Stats.ReadStats
-        verify(userController, times(1)).readStats(anyString());
+//        When - I have played four games with scores of 1,2,3, and 4
+        HashMap returnHashMap =  stats.getStatsAsHashMap();
+//        Then - totalGamesPlayed should equal 4
+        Integer result = (Integer) returnHashMap.get("totalGamesPlayed");
+        assertTrue(result==4);
     }
 
+    @Test
+    public void GivenTestListWorstScoreEqualsFour() {
+//        Given - I am an existing user
+        when(user.isExistingUser()).thenReturn(true);
+        when(user.getUsername()).thenReturn("bob");
+        when(userController.readStats(anyString())).thenReturn(testList);
+//        When - I have played four games with scores of 1,2,3, and 4
+        HashMap returnHashMap =  stats.getStatsAsHashMap();
+//        Then - Worst Score should equal 4
+        Integer result = (Integer) returnHashMap.get("worstScore");
+        assertTrue(result==4);
+    }
 
+    @Test
+    public void GivenTestListHighScoreEqualsOne() {
+//        Given - I am an existing user
+        when(user.isExistingUser()).thenReturn(true);
+        when(user.getUsername()).thenReturn("bob");
+        when(userController.readStats(anyString())).thenReturn(testList);
+//        When - I have played four games with scores of 1,2,3, and 4
+        HashMap returnHashMap =  stats.getStatsAsHashMap();
+//        Then - High Score should equal 1
+        Integer result = (Integer) returnHashMap.get("highScore");
+        assertTrue(result==1);
+    }
+
+    @Test
+    public void GivenTestListTotalShotsTakenEquals10() {
+//        Given - I am an existing user
+        when(user.isExistingUser()).thenReturn(true);
+        when(user.getUsername()).thenReturn("bob");
+        when(userController.readStats(anyString())).thenReturn(testList);
+//        When - I have played four games with scores of 1,2,3, and 4
+        HashMap returnHashMap =  stats.getStatsAsHashMap();
+//        Then - High Score should equal 1
+        Integer result = (Integer) returnHashMap.get("totalShotsTaken");
+        assertTrue(result==10);
+    }
+
+    @Test
+    public void GivenTestListAverageScoreEqualsTwoPointFive() {
+//        Given - I am an existing user
+        when(user.isExistingUser()).thenReturn(true);
+        when(user.getUsername()).thenReturn("bob");
+        when(userController.readStats(anyString())).thenReturn(testList);
+//        When - I have played four games with scores of 1,2,3, and 4
+        HashMap returnHashMap =  stats.getStatsAsHashMap();
+//        Then - High Score should equal 1
+        double result = (double) returnHashMap.get("averageScore");
+        assertTrue(result==2.5);
+    }
 
 
 }
